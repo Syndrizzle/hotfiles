fdd2346830c8608f1074325ae29915b7abc2a1ba
Sahil Nihalani <96112833+Syndrizzle@users.noreply.github.com>
2022-06-06 20:57:36 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -86,8 +86,5 @@ abbr -a -g ips 'ip link show'																					# Get network interfaces infor
 abbr -a -g wloff 'rfkill block wlan'																			# Block wlan, killing wifi connection
 abbr -a -g wlon 'rfkill unblock wlan'																		# Unblock wlan, start wifi connection
 abbr -a -g ff 'firefox'																								#
-
-cfetch
 starship init fish | source
-alias nano=nvim
 set -Ux FZF_DEFAULT_OPTS "--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
